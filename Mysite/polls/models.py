2098e5cd4f96881427487b288a185274b5603d5f
aonopko <aonopko.rpu@gmail.com>
2022-04-18 14:04:04 -0400

    add_cart

--- Mysite/polls/models.py
+++ Mysite/polls/models.py
@@ -5,12 +5,17 @@ from django.db import models
 
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(null=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    user_email = models.EmailField(null=True)
+
+
+class Category(models.Model):
+    cat_name = models.CharField(max_length=150)
+    sub_cat_name = models.CharField(max_length=150)
 
 
 class Item(models.Model):
-    item_name = models.ForeignKey('Category', on_delete=models.CASCADE)
+    item_name = models.ForeignKey(Category, on_delete=models.CASCADE)
     item_price = models.DecimalField(max_digits=15, decimal_places=3)
     item_quantity = models.IntegerField()
 
@@ -22,6 +27,5 @@ class Order(models.Model):
     order_item_name = models.ManyToManyField(Item)
 
 
-class Category(models.Model):
-    cat_name = models.CharField(max_length=150)
-    sub_cat_name = models.CharField(max_length=150)
+class Cart(models.Model):
+    user_name = (models.CharField(User, max_length=150, unique=True))
