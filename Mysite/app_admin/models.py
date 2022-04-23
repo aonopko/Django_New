db146ff4a3ed7b94cd50e161548a8497633470f1
aonopko <aonopko.rpu@gmail.com>
2022-04-23 04:30:12 -0400

    add

--- Mysite/app_admin/models.py
+++ Mysite/app_admin/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=150, unique=True)
+    name = models.CharField(max_length=255, unique=True)
     email = models.EmailField(null=True)
 
 
