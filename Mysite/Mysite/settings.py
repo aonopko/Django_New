d56bee51f6a4905022d100924eeaf5edb0a076ac
aonopko <aonopko.rpu@gmail.com>
2022-04-25 13:34:55 -0400

    add environ

--- Mysite/Mysite/settings.py
+++ Mysite/Mysite/settings.py
@@ -12,18 +12,28 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
+import environ
+
+env = environ.Env(DEBUG=(bool, False))
+
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#7aeqn3e-7un^@h*xp%60r^qc&9f5rxvbc*^bmd*uht!g9e0y5'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
--- Mysite/app_admin/models.py
+++ Mysite/app_admin/models.py
@@ -5,12 +5,12 @@ from django.db import models
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=150, unique=True)
     email = models.EmailField(null=True)
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=150)
     sub_name = models.CharField(max_length=150)
 
 
