211bf068d80cddb301a5469b6e411e484152ab1a
aonopko <aonopko.rpu@gmail.com>
2022-06-06 14:46:50 -0400

    add views cart_remove

--- Mysite/cart/urls.py
+++ Mysite/cart/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
     path('add/<int:item_id>',
          views.cart_add,
-         name='cart_add')
+         name='cart_add'),
+    path('remove/<int:item_id>/',
+         views.cart_remove,
+         name='cart_remove'),
 ]
 
 
--- Mysite/cart/views.py
+++ Mysite/cart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-from django.views.generic import TemplateView
+
 from Mysite.app_admin.models import Item
 from .cart import Cart
 from .forms import CartAddItemForm
@@ -19,9 +19,15 @@ def cart_add(request, item_id):
     return redirect('cart:cart_detail')
 
 
+def cart_remove(request, item_id):
+    cart = Cart(request)
+    item = get_object_or_404(Item, id=item_id)
+    cart.remove(item)
+    return redirect('cart:cart_detail')
+
+
 def cart_detail(request):
     cart = Cart(request)
     for item in cart:
-        item['update_quantity_form'] = CartAddItemForm(initial={'quantity': item['quantity'],
-                                                                   'update': True})
+        item['update_quantity_form'] = CartAddItemForm(initial={'quantity': item['quantity'], 'update': True})
     return render(request, 'cart/detail.html', {'cart': cart})
